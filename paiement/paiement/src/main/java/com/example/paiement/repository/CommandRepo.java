083dd36dcd321e1c1635e62579b99f3420256ac0
Nadhmi GHABI <nghabi@dom.tti>
2025-07-16 11:12:22 +0200

    EP01-delete paiementService

--- paiement/paiement/src/main/java/com/example/paiement/repository/CommandRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.paiement.repository;
-
-import com.example.paiement.domain.Command;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CommandRepo extends JpaRepository<Command, Long> {
-}
--- paiement/paiement/src/main/java/com/example/paiement/service/ProductPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.example.paiement.service;
-
-
-
-public class ProductPublisher {
-
-}
-
